7fa05a708581b023db370c009d1272487dcf4c85
dollar <13802495+dollor_com@user.noreply.gitee.com>
2024-06-12 12:36:51 +0800

    条件查询（三种传递参数的方式）

--- src/main/java/com/it/mapper/BrandMapper.java
+++ src/main/java/com/it/mapper/BrandMapper.java
@@ -1,12 +1,39 @@
 package com.it.mapper;
 
 import com.it.pojo.Brand;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+import java.util.Map;
 
 public interface BrandMapper {
 
     List<Brand> selectAll();
 
+    //根据id查询
     Brand selectById(int id);
+
+    /*
+    * 多条件查询
+    *   参数接收：
+    *           1.散装参数：如果方法中有多个参数，需要使用@Param（"Sql参数占位符名称"）
+    *           2.对象参数：对象的属性名称要和参数占位符名称一致
+    *           3.map集合参数：
+    *
+    * */
+
+    /*
+    1.散装参数
+    List<Brand> selectByCondition(@Param("status") int status,@Param("companyName") String companyName,
+                                  @Param("brandName")String brandName);*/
+
+
+   /*
+    2.封装对象参数
+    List<Brand> selectByCondition(Brand brand);*/
+
+    /*
+    3.map集合参数
+    */
+    List<Brand> selectByCondition(Map map);
 }
--- src/test/java/com/it/test/MyBatisTest.java
+++ src/test/java/com/it/test/MyBatisTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.List;
 
 public class MyBatisTest {
@@ -36,6 +37,59 @@ public class MyBatisTest {
         Brand brand = brandMapper.selectById(id);
 
         System.out.println(brand);
+
+
+        //4.释放资源
+        sqlSession.close();
+    }
+
+
+    @Test
+    public void selectByCondition() throws IOException {
+
+        //接收参数
+        int status =1;
+        String companyName = "华为";
+        String brandName = "华为";
+
+        //处理参数（模糊查询）
+        companyName  = "%" + companyName + "%";
+        brandName  = "%" + brandName + "%";
+
+        //封装对象
+       /* Brand brand = new Brand();
+        brand.setStatus(status);
+        brand.setCompanyName(companyName);
+        brand.setBrandName(brandName);*/
+
+        HashMap map = new HashMap();
+        map.put("status",status);
+        map.put("companyName",companyName);
+        map.put("brandName",brandName);
+
+        //1.加载mybatis的核心配置文件，获取sqlSessionFactory
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        //2.获取SqlSession对象，用它来执行sql
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+
+        //3.获取UserMapper接口的代理对象
+        BrandMapper brandMapper = sqlSession.getMapper(BrandMapper.class);
+
+        //4.执行方法
+        /*散装参数
+        List<Brand> brands = brandMapper.selectByCondition(status,companyName,brandName);*/
+
+        /*对象参数
+         List<Brand> brands = brandMapper.selectByCondition(brand);
+        * */
+
+        //集合参数
+        List<Brand> brands = brandMapper.selectByCondition(map);
+        System.out.println(brands);
 
 
         //4.释放资源
